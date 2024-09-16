6a5f4a33273bb62140ee340c83f61fb7692c70de
AlixGalant4 <alix.galant4@gmail.com>
2024-09-16 16:13:05 +0200

    ajout du code de base dans main

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
 import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(64, 64)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        # update des éléments du jeu
+        pass 
+    
+    def draw(self):
+        # afficher les éléments du jeu
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9) 
+
+App()
