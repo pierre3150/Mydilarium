f623fe2dcb30455bbf3a3066e2a7e41cfd82aec7
AlixGalant4 <alix.galant4@gmail.com>
2024-09-18 08:07:46 +0200

    système apparition des kjetons et mise à jours des tasks

--- Main.py
+++ Main.py
@@ -14,10 +14,10 @@ class App:
         self.SPRITES = [] # liste des éléments de notre jeu à afficher
         self.TSPRITES = [] # liste des éléments de notre jeu à afficher
         self.TEXT = [] # liste des textes de notre jeu à afficher
-        self.MAP = [Map(480, 192, 0, 0, 0, 40, 16)]
+        self.MAP = Map(480, 192, 0, 0, 0, 40, 16)
         self.INTERFACE = False # si une interface est ouverte dans le jeu
         self.TIME = '8H 10H'
-        self.task = None # Tâche actuelle
+        self.task = 0 # Tâche actuelle
         self.KEYS_PRESSED = {'UP':False, 'DOWN':False, 'LEFT':False, 'RIGHT':False}
         pyxel.init(256, 256) # dimension de la fenêtre
         pyxel.load('res.pyxres') # importation du fichier des textures
@@ -94,6 +94,11 @@ class App:
             if jeton_x-35 <= player_x+4 <= jeton_x+35 and jeton_y-35 <= player_y+4 <= jeton_y +35: 
                 jeton = pos
         return jeton
+# complète une tâche et passe à la suivante
+    def CompleteTask(self, jeton):
+        if not jeton.isComplete():
+            jeton.Complete()
+            self.task += 1
 
 # Gestion global du jeu
     def update(self):
@@ -107,9 +112,7 @@ class App:
             if pyxel.btn(pyxel.KEY_SPACE):
                 self.removeTsprite(self.getTsprites[0])
                 # on tp le joueur
-                for m in self.MAP:
-                    m.x = -470
-                    m.y = 100
+                self.MAP.x, self.MAP.y= -470, 100
                 self.setState('PLAYING') # on change l'état de la partie en PLAYING
 
         elif self.isState('PLAYING'):
@@ -117,29 +120,26 @@ class App:
             if len(self.getSprites) == 0:
                 player = Player(pyxel.width//2-4, pyxel.height//2-4, 0, 8, 0, 8, 8) # on crée un joueur
                 self.addSprite(player)
-                jeton_entree = Jeton(0, pyxel.height//2, 0, 0, 40, 8, 8, 1) # on crée des jetons interactions
-                jeton_mydil = Jeton(-518, pyxel.height//2+8, 0, 0, 40, 8, 8, 1)
-                self.addSprite(jeton_entree)
-                self.addSprite(jeton_mydil)
+
         # déplacement 
             # UP
             if not self.INTERFACE:
-                if pyxel.btn(pyxel.KEY_Z):
+                if pyxel.btn(pyxel.KEY_Z) or pyxel.btn(pyxel.KEY_O):
                     self.UP(True)
                 else:
                     self.UP(False)
                 # DOWN
-                if pyxel.btn(pyxel.KEY_S):
+                if pyxel.btn(pyxel.KEY_S) or pyxel.btn(pyxel.KEY_L):
                     self.DOWN(True)
                 else:
                     self.DOWN(False)
                 # LEFT
-                if pyxel.btn(pyxel.KEY_Q):
+                if pyxel.btn(pyxel.KEY_Q) or pyxel.btn(pyxel.KEY_K):
                     self.LEFT(True)
                 else:
                     self.LEFT(False)
                 # RIGHT
-                if pyxel.btn(pyxel.KEY_D):
+                if pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_M):
                     self.RIGHT(True)
                 else:
                     self.RIGHT(False)       
@@ -150,21 +150,105 @@ class App:
                     if len(self.getText)==1:
                         self.addText(Text(pyxel.width//3, pyxel.height//2+20, "Press 'E' to interact", 7))
                     if pyxel.btnp(pyxel.KEY_E):
-                    # on affiche l'interface du MENU
-                        x = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
-                        self.addTsprite(x)
+                    # gestion des MENUS jetons interactions
+                        if target_jeton.getNb() == 0 or target_jeton.getNb() == 1:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 2:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 3:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 4:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+                        
+                        elif target_jeton.getNb() == 5:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+                        
+                        elif target_jeton.getNb() == 6:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 7:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 8:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 9:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
+                        elif target_jeton.getNb() == 10:
+                            bulle = Image(pyxel.width//2,pyxel.height*5/6, 2, 32, 200, 32, 32, 10)
+                            self.addTsprite(bulle)
+                            # on complète la tâche
+                            self.CompleteTask(target_jeton)
+
                         self.INTERFACE = True
                 else:
                     if len(self.getText)>1:
                         self.removeText(self.getText[-1]) # supprime le dernier text
             else:
                 self.KEYS_PRESSED = {'UP':False, 'DOWN':False, 'LEFT':False, 'RIGHT':False}
+                # on retire le texte d'indication
+                if len(self.getText)>1:
+                    self.removeText(self.getText[-1]) # supprime le dernier text
                 # si on réappuye sur E on ferme le MENU
                 if pyxel.btnp(pyxel.KEY_E):
                     self.removeTsprite(self.getTsprites[-1])
                     self.INTERFACE = False
 
-        #Système d'horloge 
+        #gestion des jetons à afficher
+            if self.task == 0 and len(self.getSprites) == 1:
+                # on crée le premier jeton à l'entrée de l'EPSI
+                jeton_entree = Jeton(0, pyxel.height//2, 0, 0, 40, 8, 8, 'text', 0)   
+                self.addSprite(jeton_entree)    
+            elif self.task == 1 and len(self.getSprites) == 2:
+                jeton_mydil = Jeton(self.MAP.getPos()[0]-50, self.MAP.getPos()[1]+36, 0, 0, 40, 8, 8, 'text', 1)   
+                self.addSprite(jeton_mydil) 
+            elif self.task == 2 and len(self.getSprites) == 3:
+                jeton_reseau_cours = Jeton(self.MAP.getPos()[0]-288, self.MAP.getPos()[1]-112, 0, 0, 40, 8, 8, 'text', 2)   
+                self.addSprite(jeton_reseau_cours) 
+            elif self.task == 3 and len(self.getSprites) == 4:
+                jeton_reseau_epreuve = Jeton(self.MAP.getPos()[0]-420, self.MAP.getPos()[1], 0, 0, 40, 8, 8, 'text', 3)   
+                self.addSprite(jeton_reseau_epreuve) 
+            elif self.task == 4 and len(self.getSprites) == 5:
+                jeton_pause = Jeton(self.MAP.getPos()[0]*2-157, self.MAP.getPos()[1]*2-34, 0, 0, 40, 8, 8, 'text', 4)   
+                self.addSprite(jeton_pause)
+            elif self.task == 5 and len(self.getSprites) == 6:
+                jeton_web_cours = Jeton(self.MAP.getPos()[0]+400, self.MAP.getPos()[1]-160, 0, 0, 40, 8, 8, 'text', 5)   
+                self.addSprite(jeton_web_cours)
+            
+
+
+        #Horloge 
             if len(self.getText) == 0:
                 text = Text(pyxel.width//1-47,3, self.TIME, 7)
                 self.addText(text)
@@ -176,8 +260,7 @@ class App:
                 t.update(self.KEYS_PRESSED)
 
         # on update la map
-            for m in self.MAP:
-                m.update(self.KEYS_PRESSED)
+            self.MAP.update(self.KEYS_PRESSED)
 
         elif self.isState('FINISH'):
             pass
@@ -185,8 +268,8 @@ class App:
     def draw(self):
         pyxel.cls(0) # background noir
         ''' on affiche la map'''
-        for m in self.MAP:
-            pyxel.bltm(m.draw[0], m.draw[1], m.draw[2], m.draw[3], m.draw[4], m.draw[5]*8, m.draw[6]*8, self.cold_key, 0, 4)
+        m = self.MAP
+        pyxel.bltm(m.draw[0], m.draw[1], m.draw[2], m.draw[3], m.draw[4], m.draw[5]*8, m.draw[6]*8, self.cold_key, 0, 4)
         
         '''on parcours les sprites et les textes et on les affiche'''
         for s in self.getSprites:
@@ -196,17 +279,4 @@ class App:
         for t in self.getText:
             pyxel.text(t.draw[0], t.draw[1], t.draw[2], t.draw[3])
 
-
-
-
-
-
-
-
-
-
-
-    #Horloge interne 
-
-    
 App()
--- Sprites/Jeton.py
+++ Sprites/Jeton.py
@@ -3,7 +3,7 @@ import pyxel
 class Jeton():
     ''' affiche une texture à l'endroit souhaité'''
 # constructeur
-    def __init__(self, x, y, img, x_texture, y_texture, width, height, element) -> None:
+    def __init__(self, x, y, img, x_texture, y_texture, width, height, text, index) -> None:
         self.x = x
         self.y = y
         self.img = img
@@ -12,16 +12,23 @@ class Jeton():
         self.width = width
         self.height = height
         self.speed_scrolling = 3
-        self.element = element
+        self.text = text
+        self.index = index
 
-    def getElement(self):
-        return self.element
+    def getText(self):
+        return self.text
+    
+    def getNb(self):
+        return self.index
     
     def getPos(self):
         return self.x, self.y
     
     def Complete(self): # change le design du jeton en vert
         self.x_texture, self.y_texture = 8, 40
+
+    def isComplete(self):
+        return self.x_texture == 8 and self.y_texture == 40
     
     def update(self, keys):
         if keys['UP'] and pyxel.pget(pyxel.width//2, pyxel.height//2-14)!=0:
